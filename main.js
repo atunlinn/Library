ccf0663a7f0d979b689c361f38cdd4824a4ba622
atunlinn <aungtunlin2207@gmail.com>
2023-12-07 12:49:22 +0630

    Update Library project and use class

--- main.js
+++ main.js
@@ -1,84 +1,94 @@
 
-let myLibrary=[]
    
 
  
 
-function book(title,author,page,lang,read){
+    class Book{
+    constructor(title,author,page,lang,read){
     this.title=title;
     this.author=author;
     this.page=page;
     this.lang=lang;
     this.read=read;
   
-    
-    }
-    
-    function addBookToLibrary(){
-    
-        const title=document.querySelector("#title").value;
-    
-        const author=document.querySelector("#author").value;
-        const page=document.querySelector("#page").value;
-        const lang=document.querySelector("#lang").value;
-        const read=document.querySelector("#read").checked;
-      
-        const newBook= new book(title,author,page,lang,read)
-        myLibrary.push(newBook)
-    
-        bookInLibrary()
-    }
-          
-     const bookForm=document.getElementById("book-form");
-     bookForm.addEventListener("submit",function(e){
-        e.preventDefault();
-       
-        addBookToLibrary()
-     })
-  
- function bookInLibrary(){
-  let tbody = document.querySelector('tbody')
-  
-   tbody.innerHTML=""
-  for(let i=0;i<myLibrary.length;i++){
-
-    let b=myLibrary[i];
-    let tr = document.createElement('tr')
-    let data="<td>"+b.title+"</td><td>"+b.author+"</td><td>"+b.page+"</td><td>"+b.lang+"</td>"+
-    `<td>
-  
-    <button  onclick="updateBook(${i})">${ b.read ? "Read" : "Not Read"}</button>
-  
-    
-  
-  
-    </td>`
-   +`<td><button  onclick="deleteBook(${i})">Delete</button></td>`
-   tr.innerHTML=data;
-  tr.classList.add('book-style')
-  tbody.appendChild(tr)
-
-  }
- 
-  
- }
-  function deleteBook(index){
-    myLibrary.splice(index,1)
-    bookInLibrary()
   }
 
-
- 
- 
- book.prototype.updateBook=function (){
+  updateBook(){
     this.read= !this.read;
-  
- }
- 
+  }
 
-   function updateBook(index){
+
+    }
+    
+
+
+    class Library{
+
+     constructor(){
+      this.myLibrary=[]
+      this.bookForm=document.getElementById("book-form");
+      this.bookForm.addEventListener("submit",(e)=>{
+         e.preventDefault();
+        
+         this.addBookToLibrary()
+      })
+     
+     }
+     addBookToLibrary(){
+    
+      const title=document.querySelector("#title").value;
   
-    myLibrary[index].updateBook()
-    bookInLibrary()
+      const author=document.querySelector("#author").value;
+      const page=document.querySelector("#page").value;
+      const lang=document.querySelector("#lang").value;
+      const read=document.querySelector("#read").checked;
+    
+      const newBook= new Book(title,author,page,lang,read)
+      this.myLibrary.push(newBook)
+  
+      this.bookInLibrary()
+  }
+   
+
+  bookInLibrary(){
+    let tbody = document.querySelector('tbody')
+    
+     tbody.innerHTML=""
+    for(let i=0;i<this.myLibrary.length;i++){
+  
+      let b=this.myLibrary[i];
+      let tr = document.createElement('tr')
+      let data="<td>"+b.title+"</td><td>"+b.author+"</td><td>"+b.page+"</td><td>"+b.lang+"</td>"+
+      `<td>
+    
+      <button  onclick="library.updateBook(${i})">${ b.read ? "Read" : "Not Read"}</button>
+    
+      
+    
+    
+      </td>`
+     +`<td><button  onclick="library.deleteBook(${i})">Delete</button></td>`
+     tr.innerHTML=data;
+    tr.classList.add('book-style')
+    tbody.appendChild(tr)
+  
+    }
+   
+    
    }
+
+   deleteBook(index){
+    this.myLibrary.splice(index,1)
+    this.bookInLibrary()
+  }
+
+
+  updateBook(index){
   
+    this.myLibrary[index].updateBook()
+    this.bookInLibrary()
+   }
+
+
+    }
+  const library= new Library()
